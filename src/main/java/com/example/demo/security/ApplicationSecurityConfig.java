b2bc5a9fd4404b4524ea630d6b6b0c9c11730da4
ojuniorbezerra <junio_vjc@hotmail.com>
2020-09-14 19:29:43 -0300

    firts config

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -11,9 +11,11 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		http.authorizeRequests()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.httpBasic();
 	}
-
 	
 }
