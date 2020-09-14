19ff47da0c5cbd647bf3da8e69d8b2cc5032c0db
ojuniorbezerra <junio_vjc@hotmail.com>
2020-09-14 20:02:29 -0300

    implementation password encoder

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +9,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+		super();
+		this.passwordEncoder = passwordEncoder;
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -28,7 +38,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	@Bean
 	protected UserDetailsService userDetailsService() {
-		UserDetails user = User.builder().username("junior").password("123456").roles("STUDENTS").build();
+		UserDetails user = User.builder().username("junior").password(passwordEncoder.encode("123456")).roles("STUDENTS").build();
 		return new InMemoryUserDetailsManager(
 				user
 				);
